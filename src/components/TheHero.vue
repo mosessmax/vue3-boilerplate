743e2ae5087411325dbc375f3b854e7b98d2cd0a
Moses Adebayo <adebayomoses597@gmail.com>
2023-02-10 02:43:57 +0100

    components cleanup & small configs

---
+++ src/components/TheHero.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Hello Crackhead, this ia the home page</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import TheHero from "../components/TheHero.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <TheHero />
   </main>
 </template>
