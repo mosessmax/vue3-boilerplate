507ba17511ba6325127bb020637fef25b56adfef
Moses Adebayo <adebayomoses597@gmail.com>
2023-02-10 14:19:11 +0100

    chore: 404 included and 🚀

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>this a 404 page</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
